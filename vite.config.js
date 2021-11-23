770c47234114fa18da18cbc828372663be232830
Sam Richard <snugug@google.com>
2021-11-23 14:29:34 -0500

    🚧 (code) Get Sass working with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
+/* eslint-env node */
 const { defineConfig } = require('vite');
 const { eleventyPlugin } = require('vite-plugin-eleventy');
+const { imgPlugin } = require('vite-plugin-img');
+const { posthtmlPlugin } = require('vite-plugin-posthtml');
+const { posthtmlExternalLink } = require('posthtml-external-link');
+const path = require('path');
 
 module.exports = defineConfig({
   root: 'src',
   clearScreen: false,
   server: {
     open: true,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '$toolkit',
+        replacement: path.join(__dirname, './node_modules/sass-toolkit/stylesheets/toolkit'),
+      },
+      {
+        find: '$breakpoint',
+        replacement: path.join(__dirname, './node_modules/breakpoint-sass/stylesheets/breakpoint'),
+      },
+    ],
   },
   build: {
     outDir: '../public',
