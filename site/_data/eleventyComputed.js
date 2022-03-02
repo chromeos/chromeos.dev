9de3a1ec83d4a0777f1227e063f943edd954b330
nickalcantara <nalcantara@basicagency.com>
2022-03-02 11:51:09 -0800

    simplify grayscale palette, remove topics

--- site/_data/eleventyComputed.js
+++ site/_data/eleventyComputed.js
@@ -287,9 +287,7 @@ module.exports = {
     }
 
     if (posts && posts.length >= 1) {
-      const topics = ['technical', 'events', 'trends'];
       collections.posts = posts.map((post, i) => ({
-        topic: topics.includes(post.data.tags[0]),
         eyebrow: post.data.tags[0],
         title: post.data.title,
         body: post.data.metadesc,
