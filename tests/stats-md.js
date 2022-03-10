2b9520da15f5d67d67df3f6c99c4158598bca14e
nickalcantara <nalcantara@basicagency.com>
2022-03-10 13:19:52 -0800

    update tests, revert unintended item-grid styling

--- tests/stats-md.js
+++ tests/stats-md.js
@@ -22,28 +22,28 @@ const md = markdown({ html: true, linkify: true }).use(statsPlugin);
 
 test('Single Stat', (t) => {
   const input = '%[123%, This is a test of whats going onWorld]';
-  const output = '<p><figure class="stats"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat"><span>123%</span></dt><dd class="stats__desc type--h5">This is a test of whats going onWorld</dd></div></dl></figure></p>\n';
+  const output = '<p><figure class="stats stats--renderer"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat stats__stat--semicircle"><span>123%</span></dt><dd class="stats__desc stats__desc--renderer type--base">This is a test of whats going onWorld</dd></div></dl></figure></p>\n';
 
   t.is(md.render(input), output);
 });
 
 test('Multiple Stat', (t) => {
   const input = '%[(123%, Hello World), (456%, Bye World)]';
-  const output = '<p><figure class="stats"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat"><span>123%</span></dt><dd class="stats__desc type--h5">Hello World</dd></div><div class="stats__item"><dt class="stats__stat"><span>456%</span></dt><dd class="stats__desc type--h5">Bye World</dd></div></dl></figure></p>\n';
+  const output = '<p><figure class="stats stats--renderer"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat stats__stat--semicircle"><span>123%</span></dt><dd class="stats__desc stats__desc--renderer type--base">Hello World</dd></div><div class="stats__item"><dt class="stats__stat stats__stat--circle"><span>456%</span></dt><dd class="stats__desc stats__desc--renderer type--base">Bye World</dd></div></dl></figure></p>\n';
 
   t.is(md.render(input), output);
 });
 
 test('Three Stat', (t) => {
   const input = '%[(123%, Hello World), (456%, Bye World), (12%, This is some interesting stuff)]';
-  const output = '<p><figure class="stats"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat"><span>123%</span></dt><dd class="stats__desc type--h5">Hello World</dd></div><div class="stats__item"><dt class="stats__stat"><span>456%</span></dt><dd class="stats__desc type--h5">Bye World</dd></div><div class="stats__item"><dt class="stats__stat"><span>12%</span></dt><dd class="stats__desc type--h5">This is some interesting stuff</dd></div></dl></figure></p>\n';
+  const output = '<p><figure class="stats stats--renderer"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat stats__stat--semicircle"><span>123%</span></dt><dd class="stats__desc stats__desc--renderer type--base">Hello World</dd></div><div class="stats__item"><dt class="stats__stat stats__stat--circle"><span>456%</span></dt><dd class="stats__desc stats__desc--renderer type--base">Bye World</dd></div><div class="stats__item"><dt class="stats__stat stats__stat--triangle"><span>12%</span></dt><dd class="stats__desc stats__desc--renderer type--base">This is some interesting stuff</dd></div></dl></figure></p>\n';
 
   t.is(md.render(input), output);
 });
 
 test('2x Growth', (t) => {
   const input = '%[(55%, Growth in active installations), (2x, Growth in overall activity in app)]';
-  const output = '<p><figure class="stats"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat"><span>55%</span></dt><dd class="stats__desc type--h5">Growth in active installations</dd></div><div class="stats__item"><dt class="stats__stat"><span>2x</span></dt><dd class="stats__desc type--h5">Growth in overall activity in app</dd></div></dl></figure></p>\n';
+  const output = '<p><figure class="stats stats--renderer"><dl class="stats__list"><div class="stats__item"><dt class="stats__stat stats__stat--semicircle"><span>55%</span></dt><dd class="stats__desc stats__desc--renderer type--base">Growth in active installations</dd></div><div class="stats__item"><dt class="stats__stat stats__stat--circle"><span>2x</span></dt><dd class="stats__desc stats__desc--renderer type--base">Growth in overall activity in app</dd></div></dl></figure></p>\n';
 
   t.is(md.render(input), output);
 });
