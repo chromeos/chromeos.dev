c549458c6d0cfcb38dc8e5f6e542e49e2a631836
Vanessa Martinez <vmartinez@basicagency.com>
2022-03-22 19:39:20 -0400

    🆕(site) Add New Design to Stories Landing (#357)
    * create storiesindex page
    
    * add correct wrapper to news posts div
    
    * finished styles for stories hero
    
    * added styles for stories grid
    
    * renamed to stories-section
    
    * use item grid insteadand use clamp on image height
    
    * Update site/_generated/stories.njk
    
    Co-authored-by: Sam Richard <snugug@google.com>
    
    * changed featured one image path and changed stories image to render hero image
    
    * created featured story
    
    * clean up hero sizing
    
    * fixed featured content wrapper
    
    * add subcribe body copy
    
    * clean up, removed stories section
    
    * Update site/_components/stories-card.njk
    
    Co-authored-by: Sam Richard <snugug@google.com>
    
    * Update site/sass/components/_stories-card.scss
    
    Co-authored-by: Sam Richard <snugug@google.com>
    
    * Update site/_components/featured-story.njk
    
    Co-authored-by: Sam Richard <snugug@google.com>
    
    * fix image format
    
    Co-authored-by: Sam Richard <snugug@google.com>

--- site/_data/eleventyComputed.js
+++ site/_data/eleventyComputed.js
@@ -231,6 +231,10 @@ module.exports = {
   featured: (data) => {
     if (data.featured) {
       const featured = {
+        app: {
+          name: dataFallback('app.name')(data),
+          logo: dataFallback('app.logo')(data),
+        },
         eyebrow: dataFallback('featured.eyebrow', 'microcopy.featured.eyebrow')(data),
         title: dataFallback('featured.title', 'title')(data),
         desc: dataFallback('featured.desc', 'metadesc')(data),
@@ -306,8 +310,10 @@ module.exports = {
         .sort(propSort({ prop: 'data.title', lowercase: true }))
         .sort(propSort({ prop: 'data.weight', fallback: 0 }))
         .map((i) => ({
+          tag: i.data.tags[1],
           title: get(i, 'data.title'),
-          image: get(i, 'data.app.logo'),
+          image: get(i, 'data.hero.image'),
+          alt: get(i, 'data.hero.alt'),
           cta: {
             text: l10nFallback('microcopy.more')(data),
             url: get(i, 'data.page.url'),
