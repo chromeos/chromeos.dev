7232d02cad97ddfedf85f852be4cad38e253d943
Tim Spinks <tspinks@basicagency.com>
2022-03-24 11:25:34 -0700

    naming refactors and references

--- site/js/components/ee.js
+++ site/js/components/ee.js
@@ -11,15 +11,14 @@
  */
 export class M100 {
   constructor() {
-    this.curPrompt = this._getCurrentPrompt() || 0;
-    this.quizStart = this._getQuizStart();
+    this._current = this._getCurrent() || 0;
+    this._started = this._isStarted();
 
-    this.text = {};
-    this.text.intro = {
+    this._intro = {
       headline: 'Ace our quiz for a surprise.',
       body: ["Head on a short scavenger hunt to celebrate Chrome OS's 100th release with us. All four answers can be found here on ChromeOS.dev; get them right for a fun surprise.", "Let's go!"],
     };
-    this.text.prompts = [
+    this._prompts = [
       {
         question: 'What year did Chrome OS and the first Chromebooks launch?',
         hint: 'We wrote a blog post about it.',
@@ -49,15 +48,15 @@ export class M100 {
         answer: 4,
       },
     ];
-    this.text.reward = {
+    this._reward = {
       headline: 'Huzzah!',
       body: ["You're officially a Chrome OS insider. We've got more exciting things coming your way for our 100th software release. To stay in the know, sign up for our newsletter, and keep exploring the all-new ChromeOS.dev to help you build your next big idea.", "Thank you for all of your support, we couldn't have reached this milestone without you!", 'The Chrome OS team'],
     };
 
-    if (!this.quizStart) {
+    if (!this._started) {
       console.warn('%c%s', 'font-weight: bold;', 'm100.start()');
     } else {
-      this._promptQuestion(this.curPrompt);
+      this._promptQuestion(this._current);
     }
   }
 
@@ -66,15 +65,15 @@ export class M100 {
    * @return {void}
    */
   start() {
-    if (!this.quizStart) {
-      this._setQuizStart(true);
-      console.info('%c%s', 'font-weight: bold; background: #174ea6; color: white; padding: .25em;', this.text.intro.headline);
-      for (const iterator of this.text.intro.body) {
+    if (!this._started) {
+      this._setStarted(true);
+      console.info('%c%s', 'font-weight: bold; background: #174ea6; color: white; padding: .25em;', this._intro.headline);
+      for (const iterator of this._intro.body) {
         console.info(iterator);
       }
     }
     console.warn('Hint: to answer a question use: %c%s', 'font-family: monospace; font-weight: bold;', 'm100.answer(value);');
-    this._promptQuestion(this.curPrompt);
+    this._promptQuestion(this._current);
   }
 
   /**
@@ -83,10 +82,10 @@ export class M100 {
    * @return {void}
    */
   answer(value) {
-    if (!this.quizStart) {
+    if (!this._started) {
       console.error('Hmmm... what are you trying to answer?');
     } else {
-      this._checkAnswer(this.curPrompt, value);
+      this._checkAnswer(this._current, value);
     }
   }
 
@@ -94,18 +93,17 @@ export class M100 {
    * Start over
    * @return {void}
    */
-  clear() {
-    this._clearQuizStart();
-    this._clearCurrentPrompt();
+  restart() {
+    this._clearStarted();
+    this._clearCurrent();
   }
 
   /**
    * Gets the current prompt value from local storage.
    * @return {number} - current prompt value
    */
-  _getCurrentPrompt() {
-    const curPrompt = parseInt(localStorage.getItem('chromeos-m100-current-prompt'));
-    return curPrompt;
+  _getCurrent() {
+    return parseInt(localStorage.getItem('chromeos-m100-current-prompt'));
   }
 
   /**
@@ -113,27 +111,26 @@ export class M100 {
    * @param {number} value - the current prompt integer value
    * @return {void}
    */
-  _setCurrentPrompt(value) {
+  _setCurrent(value) {
     localStorage.setItem('chromeos-m100-current-prompt', value);
-    this.curPrompt = value;
+    this._current = value;
   }
 
   /**
    * Clear the current prompt value.
    * @return {void}
    */
-  _clearCurrentPrompt() {
+  _clearCurrent() {
     localStorage.removeItem('chromeos-m100-current-prompt');
-    this.curPrompt = 0;
+    this._current = 0;
   }
 
   /**
    * Gets the current quiz start value from local storage.
    * @return {number} - current quiz start value
    */
-  _getQuizStart() {
-    const quizStart = !!localStorage.getItem('chromeos-m100-quiz-start');
-    return quizStart;
+  _isStarted() {
+    return !!localStorage.getItem('chromeos-m100-quiz-start');
   }
 
   /**
@@ -141,18 +138,18 @@ export class M100 {
    * @param {number} value - the current quiz start integer value
    * @return {void}
    */
-  _setQuizStart(value) {
+  _setStarted(value) {
     localStorage.setItem('chromeos-m100-quiz-start', value);
-    this.quizStart = value;
+    this._started = value;
   }
 
   /**
    * Clear the current quiz start value.
    * @return {void}
    */
-  _clearQuizStart() {
+  _clearStarted() {
     localStorage.removeItem('chromeos-m100-quiz-start');
-    this.quizStart = false;
+    this._started = false;
   }
 
   /**
@@ -162,35 +159,24 @@ export class M100 {
    * @return {string|void}
    */
   _promptQuestion(prompt = 0, ret = false) {
-    if (prompt < this.text.prompts.length && this.text.prompts[prompt].question.length > 0) {
+    if (prompt < this._prompts.length && this._prompts[prompt].question.length > 0) {
       if (ret) {
-        return this.text.prompts[prompt].question;
+        return this._prompts[prompt].question;
       }
-      console.info('Question #%s: %c%s', prompt + 1, 'font-weight: bold;', this.text.prompts[prompt].question);
-    } else if ((prompt = this.text.prompts.length)) {
+      console.info('Question #%s: %c%s', prompt + 1, 'font-weight: bold;', this._prompts[prompt].question);
+    } else if ((prompt = this._prompts.length)) {
       this._promptReward();
     }
   }
 
   /**
    * Prompt the hint to the user
-   * @param {number} prompt
+   * @param {string} hint
    * @return {void}
    */
-  _promptHint(prompt) {
-    if (this.text.prompts[prompt].hint.length > 0) {
-      console.info('Hint: %c%s', 'font-style: italic;', this.text.prompts[prompt].hint);
-    }
-  }
-
-  /**
-   * Prompt the hint to the user
-   * @param {number} prompt
-   * @return {void}
-   */
-  _promptHint(prompt) {
-    if (this.text.prompts[prompt].hint.length > 0) {
-      console.info('Hint: %c%s', 'font-style: italic;', this.text.prompts[prompt].hint);
+  _promptHint(hint) {
+    if (hint) {
+      console.info('Hint: %c%s', 'font-style: italic;', hint);
     }
   }
 
@@ -199,8 +185,8 @@ export class M100 {
    * @return {void}
    */
   _promptReward() {
-    console.info('%c%s', 'font-weight: bold; font-size: 1.25em;', this.text.reward.headline);
-    for (const iterator of this.text.reward.body) {
+    console.info('%c%s', 'font-weight: bold; font-size: 1.25em;', this._reward.headline);
+    for (const iterator of this._reward.body) {
       console.info(iterator);
     }
   }
@@ -212,21 +198,22 @@ export class M100 {
    * @return {void}
    */
   _checkAnswer(prompt, value) {
-    if (value.toString().toLowerCase() == this.text.promtps[prompt].answer.toString().toLowerCase()) {
+    const prompts = this._promtps[prompt];
+    if (value.toString().toLowerCase() == prompts.answer.toString().toLowerCase()) {
       // Correct
       console.info('You answered: %c%s', 'font-weight: bold; font-style: italic; background: #0d652d; color: white; padding: .25em;', value.toString());
-      console.info(this.text.prompts[prompt].responseCorrect);
-      this._setCurrentPrompt(prompt + 1);
-      if (this.curPrompt < this.text.prompts.length) {
-        console.info('Question #%s: %c%s', this.curPrompt + 1, 'font-weight: bold;', this._promptQuestion(this.curPrompt, true));
-      } else if ((this.curPrompt = this.text.prompts.length)) {
+      console.info(prompts.responseCorrect);
+      this._setCurrent(prompt + 1);
+      if (this._current < this._prompts.length) {
+        this._promptQuestion(this._current);
+      } else if ((this._current = this._prompts.length)) {
         this._promptReward();
       }
     } else {
       // Incorrect
       console.info('You answered: %c%s', 'font-weight: bold; background: #a50e0e; color: white; padding: .25em;', value);
-      console.info('%c%s', 'font-weight: bold;', this.text.prompts[prompt].responseIncorrect);
-      this._promptHint(prompt);
+      console.info('%c%s', 'font-weight: bold;', prompts.responseIncorrect);
+      this._promptHint(prompts.hint);
     }
   }
 
