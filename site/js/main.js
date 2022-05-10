8a7c576da87ffed7c6b632edc82231ffe106efa5
Sam Richard <snugug@google.com>
2022-05-10 10:31:01 -0400

    🆕(site) Add YouTube hero option for blog posts (#426)
    * Add YouTube hero option
    
    Won't work everywhere, and images and YouTube videos are exclusive
    
    * Add YouTube display to posts, if included
    
    * Only display hero image in hero if hero is image
    
    * Progressively enhance YouTube videos to embed on click
    
    * 💚 Fix failing tests for new hero def
    
    Image is no longer strictly required for hero, it’s an and/or for image or youtube
    
    * Add and/or test for hero YouTube or Image
    
    * 💚 Fix required fields now that test is in posts

--- site/js/main.js
+++ site/js/main.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -111,6 +111,12 @@ window.addEventListener('load', async () => {
   const hero = document.querySelector('.hero-animated');
   const thankYou = document.querySelector('.thank-you');
   const tables = document.querySelectorAll('table');
+  const embeddedVideo = document.querySelectorAll('.article__video-link');
+
+  if (embeddedVideo) {
+    const { VideoHotswap } = await import('./components/video-hotswap');
+    new VideoHotswap(embeddedVideo);
+  }
 
   if (thankYou) {
     const { ThankYou } = await import('./components/thank-you');
