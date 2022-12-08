ec7b497ae748c309030908127a766cd6f5f4c827
Sam Richard <samrichard@google.com>
2022-12-08 15:34:47 -0500

    Add vite config for aliases to sanity CLI

--- cms/sanity.cli.ts
+++ cms/sanity.cli.ts
@@ -1,11 +1,23 @@
 import { defineCliConfig } from 'sanity/cli';
 import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+import process from 'process';
 
 dotenv.config();
 
 export default defineCliConfig({
   api: {
     projectId: process.env.SANITY_STUDIO_PROJECT,
-    dataset: 'production',
+    dataset: process.env.SANITY_STUDIO_API_DATASET,
+  },
+
+  vite(config) {
+    return Object.assign(config, {
+      resolve: {
+        alias: {
+          $lib: resolve(process.cwd(), 'lib'),
+        },
+      },
+    });
   },
 });
