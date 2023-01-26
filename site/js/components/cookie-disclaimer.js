19979584a3c56697a323d35ff25acbb0c205a40b
Sam Richard <samrichard@google.com>
2023-01-26 15:45:17 -0500

    🔮 is it a variable?

--- site/js/components/cookie-disclaimer.js
+++ site/js/components/cookie-disclaimer.js
@@ -49,6 +49,7 @@ export class CookieDisclaimer {
 
     if (acceptsCookies === 'true' && typeof gtag === 'function') {
       gtag('event', 'cookie_consent');
+      gtag('cookie_consent', true);
     }
   }
 
