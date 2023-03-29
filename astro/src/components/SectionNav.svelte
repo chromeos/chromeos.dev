7484fb01831809d182ea4899b71773e89a04f1a5
Sam Richard <samrichard@google.com>
2023-03-29 17:26:46 -0400

    🚧 Add section nav

---
+++ astro/src/components/SectionNav.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  interface Link {
+    title: string;
+    href: string;
+  }
+
+  export let title: string;
+  export let links: Array<Link>;
+  export let active: string;
+</script>
+
+<p class="type--h5">{title}</p>
+
+<ul>
+  {#each links as link}
+    <li>
+      <a href={link.href} data-active={active === link.href ? true : null}
+        >{link.title}</a
+      >
+    </li>
+  {/each}
+</ul>
