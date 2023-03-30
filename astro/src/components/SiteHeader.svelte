71555f0408818a2d8086165da4e21877e93dca62
Sam Richard <snugug@google.com>
2023-03-30 13:52:42 -0400

    Add placeholder site header
    For spacing purposes

---
+++ astro/src/components/SiteHeader.svelte
@@ -0,0 +1,16 @@
+<nav class="nav">
+  <h1>ChromeOS.dev</h1>
+</nav>
+
+<style lang="scss">
+  .nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 50;
+    background: white;
+    height: var(--header-height);
+    width: 100%;
+    filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.1));
+  }
+</style>
