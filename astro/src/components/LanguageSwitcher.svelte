4e88682b8df3812f99e7420fa8d4c941bddaa55d
Sam Richard <snugug@google.com>
2023-04-27 14:41:21 -0400

    Add universal footer

---
+++ astro/src/components/LanguageSwitcher.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  interface Languages {
+    code: string;
+    name: string;
+  }
+
+  export let languages: Languages[] = [];
+  export let current = '';
+  export let label = '';
+
+  let select: HTMLSelectElement;
+
+  /**
+   * Change the language of the page
+   */
+  function changeLanguage() {
+    const lang = select.value;
+    if (lang !== current) {
+      const redirect = window.location.pathname.split('/');
+      redirect.splice(1, 1, lang);
+      window.location.href = redirect.join('/');
+    }
+  }
+</script>
+
+<select
+  bind:this={select}
+  name="lang"
+  id="lang"
+  aria-label={label}
+  on:change={changeLanguage}
+  class="language-switcher type--tertiary-nav"
+>
+  {#each languages as lang}
+    <option value={lang.code} selected={lang.code === current || null}
+      >{lang.name}</option
+    >
+  {/each}
+</select>
+
+<style lang="scss">
+  .language-switcher {
+    appearance: none;
+    background-color: var(--grey-50);
+    background-image: url(/images/icons/sprite.svg#arrow-drop-down);
+    background-position: right 0% top 50%;
+    background-repeat: no-repeat;
+    background-size: 24px 24px;
+    border: 0;
+    border-bottom: 1px solid var(--grey-600);
+    border-radius: 0;
+    cursor: pointer;
+    line-height: normal;
+    padding-block: 0.4375rem;
+    padding-inline-end: 1.5rem;
+    min-width: 10ch;
+    max-width: 20ch;
+    width: 100%;
+  }
+</style>
---
+++ astro/src/components/SubscribeFooter.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { getMicrocopy } from '$lib/microcopy';
+
+  export let lang: string;
+
+  const microcopy = getMicrocopy(lang);
+</script>
+
+<div
+  class="wrapper wrapper--padding wrapper--padded wrapper--full-bleed wrapper__background--dark subscribe-footer"
+>
+  <aside class="call-out">
+    <h2 class="call-out--heading type--h2">
+      {microcopy.newsletter.subscribe.content.title}
+    </h2>
+
+    <p class="call-out--body type">
+      {microcopy.newsletter.subscribe.content.body}
+    </p>
+
+    <a class="cta cta--high" href="/{lang}/subscribe"
+      >{microcopy.newsletter.subscribe.cta.text}</a
+    >
+  </aside>
+</div>
+
+<style lang="scss">
+  .subscribe-footer {
+    width: 100%;
+  }
+
+  .call-out {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin-inline: auto;
+    margin-block: 0;
+    max-width: 65ch;
+    text-align: center;
+    gap: 1.5rem;
+
+    &--heading,
+    &--body {
+      color: var(--white);
+    }
+  }
+</style>
