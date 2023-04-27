d834d6318ef5f6c952d2f31fdfe1a6dced59333b
Sam Richard <snugug@google.com>
2023-04-27 12:32:56 -0400

    Add ability to get list of languages

--- astro/lib/microcopy.ts
+++ astro/lib/microcopy.ts
@@ -2,6 +2,8 @@ import { globSync } from 'glob';
 import { readFileSync } from 'fs';
 import { parse } from 'yaml';
 import { extname, basename } from 'path';
+import ISO6391 from 'iso-639-1';
+const countryCodes = ISO6391.getAllCodes().join('|');
 
 /**
  * Get microcopy
@@ -29,4 +31,21 @@ export function getMicrocopy(lang) {
       acc[cur.name] = cur.body;
       return acc;
     }, {});
+}
+
+/**
+ *
+ * @return {string[]} List of supported languages
+ */
+export function getLanguages() {
+  return globSync(`./src/markdown/[${countryCodes}]*`)
+    .map((f) => ({
+      code: basename(f),
+      name: ISO6391.getNativeName(basename(f)),
+    }))
+    .sort((a, b) => {
+      if (a.code < b.code) return -1;
+      if (a.code > b.code) return 1;
+      return 0;
+    });
 }
