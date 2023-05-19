ef4f7d6d78ba46a8f7ffcd0a83589529e4401e4d
Sam Richard <samrichard@google.com>
2023-05-19 16:00:36 -0400

    Add stories landing page layout

---
+++ astro/src/components/stories/FeaturedStory.svelte
@@ -0,0 +1,153 @@
+<script>
+  import CTA from '$components/CTA.svelte';
+
+  export let story = {};
+  export let microcopy = {};
+  const featured = microcopy.microcopy.featured.eyebrow;
+  const more = microcopy.microcopy.more;
+  console.log(story);
+
+  const cta = {
+    type: 'link',
+    direction: 'forward',
+    text: more,
+    url: `/` + story.path,
+  };
+</script>
+
+<article class="featured-story wrapper__contained">
+  <img
+    class="featured-story--background"
+    src={story.hero.image}
+    alt={story.hero.alt}
+  />
+  <div class="featured-story--inner ">
+    <div class="featured-story--info">
+      <img
+        class="featured-story--logo"
+        src={story.app.logo}
+        alt={story.app.name}
+      />
+      <small class="type--eyebrow featured-story--eyebrow">{featured}</small>
+      <h1 class="type--h3 featured-story--title">{story.title}</h1>
+      <div class="featured-story--cta">
+        <CTA {cta} />
+      </div>
+    </div>
+  </div>
+</article>
+
+<style lang="scss">
+  @import '$sass/shared';
+
+  .featured-story {
+    container-type: inline-size;
+    // display: grid;
+    // max-height: 690px;
+    width: 100%;
+    // height: 100%;
+    // height: 810px;
+    // overflow: hidden;
+    position: relative;
+
+    &--background {
+      display: block;
+      width: 100%;
+      height: 100%;
+      max-width: unset;
+      object-fit: cover;
+      aspect-ratio: 16/9;
+
+      @container (min-width: 612px) {
+        position: absolute;
+        top: 0;
+        right: 0;
+      }
+
+      // grid-column: 1;
+      // grid-row: 1;
+    }
+    &--inner {
+      --inner-spacing: clamp(1rem, 4cqi, 8cqi);
+      display: grid;
+      container-type: inline-size;
+
+      grid-template-rows: auto min-content;
+
+      @container (min-width: 612px) {
+        grid-template-columns: 1fr minmax(28ch, 36cqi);
+        height: 100%;
+        max-width: 1440px;
+      }
+
+      @container (min-width: 955px) {
+        grid-template-columns: 1fr 32cqi var(--inner-spacing);
+        grid-template-rows: var(--inner-spacing) auto var(--inner-spacing);
+      }
+
+      // grid-column: 1;
+      // grid-row: 1;
+    }
+
+    &--title {
+      text-align: center;
+    }
+
+    &--info {
+      // width: 32cqw;
+
+      background-color: var(--white);
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      grid-row: 1;
+      grid-column: 2;
+
+      justify-self: flex-end;
+      align-items: center;
+
+      padding: 2rem;
+      padding-block-start: 3.75rem;
+      border: 1px solid var(--grey-300);
+      border-radius: 0 0 10px 10px;
+
+      @container (min-width: 612px) {
+        padding: 2rem;
+        justify-content: center;
+        border: none;
+        border-radius: 0;
+      }
+
+      @container (min-width: 955px) {
+        grid-row: 2;
+        border-radius: 10px;
+        max-width: 500px;
+      }
+      // height: 100%;
+    }
+
+    &--logo {
+      @include elevation(2);
+      border-radius: 0.625rem;
+      height: 3.75rem;
+      margin: 0 auto;
+      // transform: translateY(-50%);
+      width: 3.75rem;
+      margin-bottom: 1rem;
+      position: absolute;
+      top: -1.875rem;
+
+      @container (min-width: 612px) {
+        position: unset;
+      }
+    }
+
+    &--cta {
+      // align-self: flex-end;
+
+      @container (min-width: 612px) {
+        margin-top: 2rem;
+      }
+    }
+  }
+</style>
---
+++ astro/src/components/stories/StoryCard.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+  import crypto from 'crypto';
+  import CTA from '$components/CTA.svelte';
+
+  interface StoryPreview {
+    title: string;
+    metadesc: string;
+    path: string;
+    lang: string;
+    date: Date;
+    app: {
+      name: string;
+      logo: string;
+      company: string;
+    };
+    hero: {
+      image: string;
+      alt: string;
+    };
+    featurd: boolean;
+    tags: string[];
+  }
+
+  export let story: StoryPreview;
+
+  const id = crypto.randomUUID().replace(/-/g, '').substring(0, 7);
+  const image = story?.hero?.image || placeholder();
+
+  /**
+   * Determines correct placeholder image to use
+   * @return {string} Placeholder image
+   */
+  function placeholder() {
+    const t = story.tags[0];
+    if (t === 'pwa') {
+      return 'ix://landings/stories/placeholder-pwa.svg';
+    } else if (t === 'android') {
+      return 'ix://landings/stories/placeholder-android.svg';
+    } else {
+      return 'ix://landings/stories/placeholder=games.svg';
+    }
+  }
+
+  const cta = {
+    type: 'link',
+    direction: 'forward',
+    text: 'Read more',
+  };
+</script>
+
+<a href={story.path} class="story-card" aria-labelledby={id}>
+  <article class="story-card--container">
+    <img src={image} alt={story?.hero?.alt || null} />
+    <div class="story-card--content">
+      <h2 class="story-card--title type--h4" {id}>{story.title}</h2>
+      <CTA {cta} inline={true} />
+    </div>
+  </article>
+</a>
+
+<style lang="scss">
+  @import '$sass/shared';
+
+  .story-card {
+    display: block;
+    text-decoration: none;
+    container-type: inline-size;
+
+    &--container {
+      @include elevation(0);
+      background-color: var(--white);
+      border-radius: 0.625rem;
+      display: flex;
+      flex-flow: column;
+      height: 100%;
+      overflow: hidden;
+      position: relative;
+      text-decoration: none;
+      width: 100%;
+      z-index: 1;
+
+      &:focus,
+      &:hover {
+        @include elevation(2);
+      }
+
+      > :global(img) {
+        aspect-ratio: 16 / 9;
+        width: 100%;
+        object-fit: cover;
+      }
+    }
+
+    &--title {
+      // font-size: clamp(1.125rem, 4.5cqw, 1.375rem);
+    }
+
+    &--content {
+      padding: clamp(1.5rem, 6.5cqw, 2.5rem);
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      gap: 1.25rem;
+      height: 100%;
+    }
+  }
+</style>
