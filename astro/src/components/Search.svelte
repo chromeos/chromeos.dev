73a9fe1c6ecce6c5f64ed9bcb75ae9565745570b
Sam Richard <snugug@google.com>
2023-06-08 12:39:47 -0400

    Make Search reusable

---
+++ astro/src/components/Search.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import type { Locale } from '$types/content';
+  export let locale: Locale;
+  export let label;
+</script>
+
+<form action={`/${locale.code}/search`} class="search">
+  <input
+    class="search--input"
+    type="search"
+    name="q"
+    aria-label={label}
+    placeholder={label}
+  />
+  <input type="hidden" name="locale" value={locale.code} />
+  <input type="hidden" name="page" value="1" />
+  <button aria-label={label} class="search--button">
+    <svg role="img" aria-hidden="true" class="icon">
+      <use href="/images/icons/sprite.svg#search" />
+    </svg>
+  </button>
+</form>
+
+<style lang="scss">
+  .search {
+    display: grid;
+    grid-template-columns: 1.5rem auto;
+    grid-auto-flow: dense;
+    gap: 0.5rem;
+    background: var(--white);
+    padding: 0.5rem;
+    width: 100%;
+    // max-width: 50ch;
+
+    &--input {
+      grid-column: 2;
+      background: none;
+      border: 0;
+    }
+
+    &--button {
+      padding: 0;
+      border: 0;
+      background: none;
+    }
+  }
+</style>
