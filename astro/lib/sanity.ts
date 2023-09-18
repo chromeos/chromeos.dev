0bcca6e9ee41fd261a6bac1268c84971845fa25d
Sam Richard <samrichard@google.com>
2023-09-18 14:44:38 -0400

    🚧 Post, Microcopy, and Languages queries and types

--- astro/lib/sanity.ts
+++ astro/lib/sanity.ts
@@ -1,5 +1,6 @@
 import { useSanityClient } from '@sanity/astro';
 import { normalizeLang } from '$$data';
+import iso6391 from 'iso-639-1';
 import * as path from 'path';
 import { inspect } from 'util';
 
@@ -37,6 +38,7 @@ async function groq(strings: TemplateStringsArray, ...keys: any[]) {
     /}$/g,
     `_type,
     '_lang': coalesce(__i18n_lang, 'en_US'),
+    '_langCode': string::split(coalesce(__i18n_lang, 'en_US'), '_')[0],
     'dates': {
       'published': coalesce(date_overrides.published, _createdAt),
       'updated': coalesce(date_overrides.updated, date_overrides.published, _updatedAt)
@@ -44,39 +46,107 @@ async function groq(strings: TemplateStringsArray, ...keys: any[]) {
   );
 
   // Clean up results
-  return (await sanity.fetch(query)).map((item) => {
-    // Clean up dates
-    if (item.dates.published === item.dates.updated) {
-      delete item.dates.updated;
-    }
-    item.dates.published = new Date(item.dates.published);
-    if (item.dates.updated) {
-      item.dates.updated = new Date(item.dates.updated);
-    }
+  return (await sanity.fetch(query))
+    .map((item) => {
+      // Clean up dates
+      if (item.dates.published === item.dates.updated) {
+        delete item.dates.updated;
+      }
+      item.dates.published = new Date(item.dates.published);
+      if (item.dates.updated) {
+        item.dates.updated = new Date(item.dates.updated);
+      }
 
-    // Normalize sections
-    // TODO: Standardize
-    if (item._type === 'post') {
-      item._section = 'news';
-    }
+      // Normalize sections
+      // TODO: Standardize
+      if (item._type === 'post') {
+        item._section = 'news';
+      }
 
-    // Build path
-    // TODO: Standardize
-    item._path = path.join(
-      '/',
-      normalizeLang(item._lang),
-      item._section,
-      item.slug,
-    );
-    return item;
-  });
+      // Build path
+      // TODO: Standardize
+      item._path = path.join(
+        '/',
+        normalizeLang(item._lang),
+        item._section,
+        item._slug,
+      );
+      return item;
+    })
+    .sort((a, b) => {
+      // Sort by date.published
+      return b.dates.published - a.dates.published;
+    })
+    .reduce((acc, cur) => {
+      // Sort into language groups
+      if (!acc[cur._lang]) {
+        acc[cur._lang] = [];
+      }
+      acc[cur._lang].push(cur);
+      return acc;
+    }, {});
 }
 
-export const languages = (
+// Languages
+const rtl = ['ar', 'dv', 'ae', 'fa', 'he', 'ps', 'ja', 'ur', 'ko', 'zh'];
+const vertical = ['zh', 'mh', 'ja', 'ko'];
+
+export const buildLink = () => {
+  return `text,
+  url.reference._type == 'reference' => {
+    'url': {
+      'slug': url.reference->slug.current,
+      'type': url.reference->_type,
+    }
+  },
+  url.reference._type != 'reference' => {
+    'url': url.url,
+  },`;
+};
+
+export const microcopy = (
   await sanity.fetch(
-    `*[_type == 'microcopy' && !(_id in path('drafts.**'))]{__i18n_lang}`,
+    `*[_type == 'microcopy' && !(_id in path('drafts.**'))]{
+      ...,
+      'footer': {
+        'links': footer.links[] {
+          ${buildLink()}
+        },
+        'language': footer.language,
+        'help': footer.help {
+          ${buildLink()}
+        },
+      },
+      '_lang': coalesce(__i18n_lang, 'en_US'),
+      'locale': {
+        'code': string::split(coalesce(__i18n_lang, 'en_US'), '_')[0],
+      }
+    }`,
   )
-).map((lang) => lang.__i18n_lang);
+)
+  .map((m) => {
+    delete m._id;
+    delete m._type;
+    delete m._rev;
+    delete m._createdAt;
+    delete m._updatedAt;
+    delete m.__i18n_lang;
+
+    // Build locale data
+    m.locale.dir = rtl.includes(m.locale.code) ? 'rtl' : 'ltr';
+    m.locale.vertical = vertical.includes(m.locale.code);
+    m.locale.name = iso6391.getNativeName(m.locale.code);
+    return m;
+  })
+  // Reduce into an object of languages
+  .reduce((acc, cur) => {
+    console.log(cur);
+    acc[cur?._lang] = cur;
+    return acc;
+  }, {});
+
+// Get languages
+export const languages = Object.keys(microcopy);
 
 export type Postcard = {
   title: string;
@@ -90,21 +160,36 @@ export type Postcard = {
   };
 };
 
-export const microcopy = (
-  await sanity.fetch(`*[_type == 'microcopy' && !(_id in path('drafts.**'))]`)
-)
-  .map((m) => {
-    delete m._id;
-    delete m._type;
-    delete m._rev;
-    delete m._createdAt;
-    delete m._updatedAt;
-    return m;
-  })
-  .reduce((acc, cur) => {
-    acc[cur?.__i18n_lang] = cur;
-    return acc;
-  }, {});
+export type StaticPathCallback = {
+  params: Record<string, any>;
+  props: Record<string, any>;
+};
+
+/**
+ *
+ * @param {function} cb
+ * @return {Promise<StaticPathCallback[]>}
+ */
+export function buildStaticPaths(
+  cb: (lang: string) => Promise<StaticPathCallback>,
+) {
+  const paths = Object.entries(microcopy).map(async ([lang, m]) => {
+    const { params, props } = await cb(lang);
+
+    // const;
+
+    return {
+      params: Object.assign(params, {
+        lang,
+      }),
+      props: Object.assign(props, {
+        microcopy: m,
+      }),
+    };
+  });
+
+  return Promise.all(paths);
+}
 
 /**
  * Microcpy query mixin for GROQ
@@ -116,16 +201,17 @@ function groqMicrocopy(lang: string, property: string) {
   return `*[_type == 'microcopy' && !(_id in path('drafts.**')) && (__i18n_lang == ${lang} || __i18n_lang == 'en_US')]{'item': ${property}}[0].item`;
 }
 
+// Uber Body query.
+
 /**
  * Get post
  * @return {object}
  */
 export async function getPosts() {
-  const q = (
-    await groq`*[_type == "post"]{
+  const q = await groq`*[_type == "post"]{
       title,
       description,
-      'slug': slug.current,
+      '_slug': slug.current,
       // body,
       category->{
         title,
@@ -134,22 +220,27 @@ export async function getPosts() {
       author[]->{
         name,
         work,
-        'image': image.asset->url
+        'image': image.asset
       },
       tags[]->{
         title,
         'slug': slug.current
       },
       hero.include == true => {
-        'hero': coalesce({
-          'youtube': hero.hero.youtube,
-        }, {
-          'image': hero.hero.image.asset->url,
-          'alt': hero.hero.image.alt,
-        })
+        'hero': coalesce(
+          {
+           'youtube': hero.hero.youtube.id,
+          },
+          {
+            'image': hero.hero.image.asset,
+           'alt': hero.hero.image.alt,
+          }
+        )
       },
       'theme': {
-        'icon': coalesce(theme.theme->icon.asset->url, 'https://chromeos-dev.imgix.net/icons/eyebrows/' + category->slug.current + '.svg?auto=format,compress'),
+        'icon': coalesce(
+          theme.theme->icon.asset,
+          'https://chromeos-dev.imgix.net/icons/eyebrows/' + category->slug.current + '.svg?auto=format,compress'),
         'slug': coalesce(theme.theme->slug.current, category->slug.current),
         featured.feature == true => {
           'eyebrow': coalesce(
@@ -166,34 +257,31 @@ export async function getPosts() {
         },
         'backgrounds': {
           'large': coalesce(
-            theme.backgrounds.background_large.asset->url,
-            theme.theme->background_large.asset->url,
-            'https://chromeos-dev.imgix.net/landings/news/top/banner-' + category-> slug.current + '.svg?auto=format,compress'
+           theme.backgrounds.background_large.asset,
+           theme.theme->background_large.asset,
+            'ix://landings/news/top/banner-' + category-> slug.current + '.svg?auto=format,compress'
           ),
           'small': coalesce(
-            theme.backgrounds.background_small.asset->url,
-            theme.theme->background_small.asset->url,
-            'https://chromeos-dev.imgix.net/landings/news/banner-' + category-> slug.current + '.svg?auto=format,compress'
+           theme.backgrounds.background_small.asset,
+           theme.theme->background_small.asset,
+            'ix://landings/news/banner-' + category-> slug.current + '.svg?auto=format,compress'
           ),
         }
       },
-      share,
+      // share,
       featured.feature == true => {
         'featured': {
           'title': coalesce(featured.featured.title, title),
           'description': coalesce(featured.featured.description, description),
           coalesce(featured.featured.image, hero.hero.image) != null => {
             'media': {
-              'image': coalesce(featured.featured.image.asset->url, hero.hero.image.asset->url),
+              'image': coalesce(featured.featured.image.asset, hero.hero.image.asset),
               'alt': coalesce(featured.featured.image.alt, hero.hero.image.alt),
             }
           }
         }
       },
-    }`
-  ).map((post) => {
-    return post;
-  });
+    }`;
 
   return q;
 }
@@ -208,8 +296,8 @@ export async function getPosts() {
 //   category->title
 // ),
 // 'backgrounds': {
-//   'large': coalesce(theme.backgrounds.background_large.asset->url, theme.theme->background_large.asset->url, 'https://chromeos-dev.imgix.net/landings/news/top/banner-' + category-> slug.current + '.svg?auto=format,compress'),
-//   'small': coalesce(theme.backgrounds.background_small.asset->url, theme.theme->background_small.asset->url, 'https://chromeos-dev.imgix.net/landings/news/banner-' + category-> slug.current + '.svg?auto=format,compress'),
+//   'large': coalesce(theme.backgrounds.background_large.asset, theme.theme->background_large.asset, 'https://chromeos-dev.imgix.net/landings/news/top/banner-' + category-> slug.current + '.svg?auto=format,compress'),
+//   'small': coalesce(theme.backgrounds.background_small.asset, theme.theme->background_small.asset, 'https://chromeos-dev.imgix.net/landings/news/banner-' + category-> slug.current + '.svg?auto=format,compress'),
 // }
 
 /**
@@ -221,9 +309,10 @@ export async function getCardData(type: string) {
   // const q = `*[_type == "${type}"  && !(_id in path('drafts.**'))]{title, description, 'type': _type, 'slug': slug.current, 'lang': __i18n_lang, category->{title, 'slug': slug.current}}`;
   // console.log(microcopy);
   const q = await getPosts();
+  const featured = q.find((post) => post?.featured);
 
   // const posts = await buildPostsFromAPI(q);
-  console.log(inspect(q[q.length - 1], false, null, true));
+  console.log(inspect(featured, false, null, true));
   // const cards = (await sanity.fetch(query)).map((card) => {
   //   return card;
   // });
--- astro/types/sanity.d.ts
+++ astro/types/sanity.d.ts
@@ -27,24 +27,132 @@ export type YouTube = {
   youtube: string;
 };
 
+export type Backgrounds = {
+  large: string;
+  small: string;
+};
+
+export type Theme = {
+  icon: string;
+  slug: string;
+  eyebrow: string;
+  backgrounds: Backgrounds;
+};
+
+export type Featured = {
+  title: string;
+  description: string;
+  media: Image;
+};
+
 export type Hero = YouTube | Image;
 
 export type Post = {
   title: string;
   description: string;
-  slug: string;
   body: BlockSchemaType;
   category: Tag;
   author: Author[];
   tags: Tag[];
   hero?: Hero;
-
+  theme: Theme;
+  featured?: Featured;
   dates: {
     published: Date;
     updated?: Date;
   };
+  _slug: string;
   _type: string;
   _lang: string;
+  _langCode: string;
   _section: string;
   _path: string;
+};
+
+export type CMSLinkReference = {
+  url: {
+    slug: string;
+    type: string;
+  };
+};
+
+export type CMSLink = {
+  text: string;
+  url: string | CMSLinkReference;
+};
+
+export type Microcopy = {
+  _lang: string; // Full language code, with country, if applicable
+  accessibility: {
+    skip: {
+      toc: string; // Skip to table of contents
+      content: string; // Skip to main content
+    };
+    authorAlt: string; // ((a)) - Replace author name
+  };
+  actions: {
+    reset: string;
+    loadVideo: string;
+    remove: string;
+    required: string;
+    add: string;
+    subscribe: string;
+    more: string; // Read More
+    back: string; // Breadcrumb back ((a)) - Replace with page title
+  };
+  meta: {
+    authored: string;
+    toc: string;
+    updated: string;
+    posted: string;
+    tools: {
+      title: string;
+      versions: {
+        singular: string;
+        plural: string;
+      };
+    };
+  };
+  network: {
+    offline: {
+      title: string;
+      description: string;
+    };
+    sw: {
+      reload: {
+        message: string;
+        action: string;
+      };
+      refresh: {
+        message: string;
+        action: string;
+      };
+    };
+    notFound: string;
+  };
+  topics: {
+    title: string;
+    related: string;
+    section: string;
+  };
+  footer: {
+    links: CMSLink[];
+    help: CMSLink;
+    language: string;
+  };
+  pagination: {
+    next: string;
+    previous: string;
+    current: string;
+    first: string;
+    last: string;
+    page: string;
+    title: string;
+  };
+  locale: {
+    code: string;
+    name: string;
+    dir: string;
+    vertical: boolean;
+  };
 };
