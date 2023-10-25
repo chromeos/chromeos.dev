00b4aff5efe3fc988aac7d729a92de1cd362e0ce
Sam Richard <samrichard@google.com>
2023-10-25 11:59:51 -0400

    Add heading and picture rendering

--- astro/src/components/blocks/Heading.svelte
+++ astro/src/components/blocks/Heading.svelte
@@ -1,36 +1,29 @@
 <script lang="ts">
-  import { PortableText } from '@portabletext/svelte';
-  import type { CustomBlockComponentProps } from '@portabletext/svelte';
+  import type { PortableTextObject } from '@sanity/types';
+  import { slugify } from '$lib/data';
 
-  export let portableText: CustomBlockComponentProps<{ style?: boolean }>;
+  export let block: PortableTextObject;
 
   /**
    * Get the text from a PortableText node
-   * @param {Object} child
+   * @param {PortableTextObject} child
    * @return {string}
    */
-  function getHeaderText(child) {
+  function getHeaderText(child: PortableTextObject) {
     let content = '';
     if (child.text) {
-      content = child.text;
+      content = child.text as string;
     } else if (child.children) {
-      child.children.forEach((child) => {
-        content += getHeaderText(child);
-      });
+      content += child.children.map(getHeaderText).join('') as string;
     }
     return content;
   }
 
-  const text = getHeaderText(portableText.value);
-  const slug = '#' + text.toLocaleLowerCase().replace(/ /g, '-');
-  const style = portableText.value.style;
-  console.log(portableText.value);
+  const style = block?.style as 'string';
+  const text = getHeaderText(block);
+  const id = slugify(text);
 </script>
 
-<svelte:element this={style} id={slug.substring(1)}
-  ><a href={slug}>
-    {#each portableText.value.children as child}
-      <PortableText value={child} />
-    {/each}
-  </a></svelte:element
+<svelte:element this={style} {id} class="header-anchor--wrapper">
+  <a href="#{id}" class="header-anchor">{text}</a></svelte:element
 >
---
+++ astro/src/components/blocks/Picture.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  export let block: any;
+</script>
+
+<img src="cms://{block.asset._ref}" alt={block.alt} />
