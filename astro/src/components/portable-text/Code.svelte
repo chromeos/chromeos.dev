97d37226bade22d026e79de3d0c9a91b8c85966e
Sam Richard <samrichard@google.com>
2023-11-21 15:49:08 -0500

    Fix for unknown languages

--- astro/src/components/portable-text/Code.svelte
+++ astro/src/components/portable-text/Code.svelte
@@ -5,12 +5,21 @@
 
   const { code, language, filename } = block;
   const cls = `language--${language}`;
+
+  let highlighted;
+  let l = language;
+
+  try {
+    highlighted = highlight(code, l);
+  } catch (e) {
+    l = 'text';
+    highlighted = highlight(code, l);
+  }
 </script>
 
 <figure class="code-figure">
-  <figcaption class="type--label">{filename || language}</figcaption>
-  <pre class={cls}><code class={cls}>{@html highlight(code, language)}</code
-    ></pre>
+  <figcaption class="type--label">{filename || l}</figcaption>
+  <pre class={cls}><code class={cls}>{@html highlighted}</code></pre>
 </figure>
 
 <style lang="scss">
