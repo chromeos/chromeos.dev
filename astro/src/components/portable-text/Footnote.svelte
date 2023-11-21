bb36ac5ef6e69888dde97e236ecefeee33891500
Sam Richard <samrichard@google.com>
2023-11-21 17:01:00 -0500

    Add support for footnotes

---
+++ astro/src/components/portable-text/Footnote.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let block: any;
+  const href = `#fn-${block.markDef.id}`;
+  const id = `fnref-${block.markDef.id}`;
+</script>
+
+<slot />
+<sup>
+  <a {href} {id}>[{block.markDef.id}]</a>
+</sup>
