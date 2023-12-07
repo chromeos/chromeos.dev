960bc6568469330a72e142cb5a1cfe42d3743a78
Sam Richard <samrichard@google.com>
2023-12-07 10:57:01 -0500

    Make featured post respond properly to large sized screens

---
+++ astro/src/components/home/FeaturedPost.svelte
@@ -0,0 +1,18 @@
+<script>
+  import PostHero from '$components/PostHero.svelte';
+  export let featured = {};
+</script>
+
+<section class="wrapper wrapper__contained wrapper--no-bottom wrapper--no-top">
+  <PostHero {...featured} />
+</section>
+
+<style lang="scss">
+  @import '$sass/globals/vars';
+
+  .wrapper {
+    @media (min-width: #{$wrapper-max-width}) {
+      margin-block-end: calc($wrapper-block-spacing-r / 2);
+    }
+  }
+</style>
