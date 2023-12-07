9a04d2fc7a26b7a859a184ab11f4697a55d23b8c
Sam Richard <samrichard@google.com>
2023-12-07 13:56:43 -0500

    Update stats regex

--- cms/schemas/fields/statistic.ts
+++ cms/schemas/fields/statistic.ts
@@ -28,7 +28,7 @@ export default defineType({
         Rule.custom((value, { parent }) => {
           if (parent?.description && !value) return 'Statistic is required';
           return true;
-        }).regex(/^[+~><]?(\d+)[Mkx%+]?$/),
+        }).regex(/^[+~><]?(\d+.?\d*)[Mk%]?[x+]?$/),
     }),
     defineField({
       name: 'description',
