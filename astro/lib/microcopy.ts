fcf543e82cdf04de3e460d40098ddf419545db73
Sam Richard <snugug@google.com>
2023-12-11 16:39:31 -0500

    Rename getMicrocopyFromLangCode to getMicrocopy

--- astro/lib/microcopy.ts
+++ astro/lib/microcopy.ts
@@ -2,11 +2,10 @@ import type { Microcopy } from '$types/sanity';
 import { microcopy } from '$$sanity';
 
 /**
- * TODO: Swap this to just getMicrocopy once all instances of that function have been converted
  * Get microcopy from locale code
  * @param {string} code - Locale code
  * @return {Microcopy}
  */
-export function getMicrocopyFromLangCode(code: string): Microcopy {
+export function getMicrocopy(code: string): Microcopy {
   return Object.values(microcopy).find((m) => m.locale.code === code);
 }
