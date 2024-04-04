f7203576f5d158eacfd8907c44b7e51f65edd517
Sam Richard <samrichard@google.com>
2024-04-04 11:35:21 -0400

    Release notes for microcopy

--- cms/schemas/microcopy.tsx
+++ cms/schemas/microcopy.tsx
@@ -587,6 +587,13 @@ export default defineType({
           validation: (Rule) => Rule.required(),
         }),
         defineField({
+          name: 'notesfor',
+          title: 'Release Notes for',
+          description: '((v)) - Version',
+          type: 'string',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
           name: 'stable',
           title: 'Stable',
           type: 'string',
