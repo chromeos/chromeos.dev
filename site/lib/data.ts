c098df8e7b23b1dfb40b82549651c9cebd3a3366
Sam Richard <samrichard@google.com>
2024-05-06 15:37:46 -0400

    Let stringify take a PortableTextBlock

--- site/lib/data.ts
+++ site/lib/data.ts
@@ -1,4 +1,9 @@
 import slug from 'slugify';
+import { toText } from '$lib/portabletext';
+
+slug.extend({
+  '+': 'plus',
+});
 
 /**
  * Normalizes a language string to the first part of the string
@@ -14,8 +19,15 @@ export function normalizeLang(lang: string) {
  * @param {string} str
  * @return {string}
  */
-export function slugify(str: string) {
-  let s = slug(str, { lower: true, strict: true });
+export function slugify(str: string | PortableTextBlock[]) {
+  let holder = str;
+  if (typeof str !== 'string') {
+    holder = toText(str);
+  }
+
+  holder = typeof holder === 'string' ? holder : '';
+
+  let s = slug(holder, { lower: true, strict: true });
   // If it doesn't start with a valid character, add an underscore
   if (!s.match(/^([a-z]|_)/)) {
     s = `_${s}`;
