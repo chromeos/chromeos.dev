991109b81245a361752e8dcb00936281aa416f29
Sam Richard <snugug@google.com>
2024-08-23 14:36:44 -0400

    🐛(site) Include tutorial links and software in docs (#774)
    * Add tutorials to section nav
    
    * Add software to documentation pages
    
    ---------
    
    Co-authored-by: Sam Richard <samrichard@google.com>

--- site/lib/documentation.ts
+++ site/lib/documentation.ts
@@ -1,11 +1,11 @@
 import type { Tag } from '$types/sanity';
-import { documentation, landings } from '$lib/sanity';
+import { documentation, landings, tutorials } from '$lib/sanity';
 
 /**
  * Build documentation sections
  * @param {Tag} category
  * @param {string} lang
- * @return {object}
+ * @return {object[]}
  */
 export function buildSection(category: Tag, lang: string = 'en') {
   const sections = documentation
@@ -30,6 +30,23 @@ export function buildSection(category: Tag, lang: string = 'en') {
 }
 
 /**
+ * Build documentation tutorials
+ * @param {Tag} category
+ * @param {string} lang
+ * @return {object[] | null}
+ */
+export function buildTutorials(category: Tag, lang: string = 'en') {
+  const tuts = tutorials
+    .filter((l) => l._lang === lang)
+    .filter((l) => l.category.slug === category.slug)
+    .map((l) => ({
+      title: l.title,
+      href: l._path,
+    }));
+  return tuts.length > 0 ? tuts : null;
+}
+
+/**
  * Build documentation topics
  * @param {string[]} paths Array of paths
  * @return {Object}
@@ -37,6 +54,7 @@ export function buildSection(category: Tag, lang: string = 'en') {
 export function buildTopics(paths: string[]) {
   return documentation
     .filter((d) => paths.includes(d._path))
+    .sort((a, b) => a.title.localeCompare(b.title))
     .sort((a, b) => a.weight - b.weight)
     .map((d) => ({
       title: d.title,
